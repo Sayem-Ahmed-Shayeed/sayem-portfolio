25c5d093efeae068008c276227ce8bde0483fcd2
Sayem Ahmed Shayeed <shayeedahmed2@gmail.com>
2026-09-09 19:07:11 +0600

    ci: configure GitHub Pages project deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://sayem-ahmed-shayeed.github.io',
+  base: '/sayem-portfolio',
   output: 'static',
   build: { format: 'directory' }
 });
